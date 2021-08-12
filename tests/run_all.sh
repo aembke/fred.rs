45ae18a1ea5f60775c0a8655c2516271667ccd00
Alec Embke <aembke@gmail.com>
2021-08-11 22:13:47 -0700

    ci: test build

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export RUST_BACKTRACE=full
 cargo test --release -- --test-threads=1
 cargo test --release --no-default-features -- --test-threads=1
 cargo test --release --all-features -- --test-threads=1
