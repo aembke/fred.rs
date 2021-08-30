8646a13c3790aa9514e3d1f2045d1ae2ef6e3f67
Alec Embke <aembke@gmail.com>
2021-08-29 18:46:10 -0700

    test: fix ci test command

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -3,4 +3,6 @@
 export RUST_BACKTRACE=full
 cargo test --release --lib --tests -- --test-threads=1
 cargo test --release --lib --tests --no-default-features -- --test-threads=1
-cargo test --release --lib --tests --all-features -- --test-threads=1
+cargo test --release --lib --tests --features \
+  "index-map network-logs pool-prefer-active enable-tls vendored-tls custom-reconnect-errors ignore-auth-error blocking-encoding full-tracing reconnect-on-auth-error" \
+  -- --test-threads=1
