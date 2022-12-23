ee6d3c18c1d472be0893ac3c70334e07ab8f0cba
Alec Embke <aembke@gmail.com>
2022-12-22 16:57:16 -0800

    feat: add watch_before to trx interface

--- src/commands/interfaces/transactions.rs
+++ src/commands/interfaces/transactions.rs
@@ -1,9 +1,4 @@
-use crate::{
-  clients::Transaction,
-  commands,
-  interfaces::{ClientLike, RedisResult},
-  types::MultipleKeys,
-};
+use crate::{clients::Transaction, interfaces::ClientLike};
 
 /// Functions that implement the [transactions](https://redis.io/commands#transactions) interface.
 ///
@@ -15,23 +10,5 @@ pub trait TransactionInterface: ClientLike + Sized {
   /// <https://redis.io/commands/multi>
   fn multi(&self) -> Transaction {
     self.inner().into()
-  }
-
-  /// Marks the given keys to be watched for conditional execution of a transaction.
-  ///
-  /// <https://redis.io/commands/watch>
-  async fn watch<K>(&self, keys: K) -> RedisResult<()>
-  where
-    K: Into<MultipleKeys> + Send,
-  {
-    into!(keys);
-    commands::keys::watch(self, keys).await
-  }
-
-  /// Flushes all the previously watched keys for a transaction.
-  ///
-  /// <https://redis.io/commands/unwatch>
-  async fn unwatch(&self) -> RedisResult<()> {
-    commands::keys::unwatch(self).await
   }
 }
