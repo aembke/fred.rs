7f5ab643861a228a220d9fdf6c4940c05f5be897
Alec Embke <aembke@gmail.com>
2022-12-26 19:13:41 -0800

    ci: add base image runner

---
+++ tests/runners/docker-bash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# boot all the redis servers and start a bash shell on a new container
+docker-compose -f tests/docker/compose/cluster-tls.yml -f tests/docker/compose/centralized.yml -f tests/docker/compose/cluster.yml \
+  -f tests/docker/compose/sentinel.yml -f tests/docker/compose/base.yml run -u $(id -u ${USER}):$(id -g ${USER}) --rm base
