aa7fc0271b526f41ace297a6576b775bcd410c06
Julian Kovacek <julian.kovacek@outlook.com>
2023-11-17 13:58:49 +1100

    7.0.1 (#184)
    * fix: don't panic when jitter is 0

--- src/utils.rs
+++ src/utils.rs
@@ -565,7 +565,11 @@ where
 }
 
 pub fn add_jitter(delay: u64, jitter: u32) -> u64 {
-  delay.saturating_add(rand::thread_rng().gen_range(0 .. jitter as u64))
+  if jitter == 0 {
+    delay
+  } else {
+    delay.saturating_add(rand::thread_rng().gen_range(0 .. jitter as u64))
+  }
 }
 
 pub fn into_redis_map<I, K, V>(mut iter: I) -> Result<HashMap<RedisKey, RedisValue>, RedisError>
@@ -830,6 +834,11 @@ mod tests {
 
   fn a(v: Vec<RedisValue>) -> RedisValue {
     RedisValue::Array(v)
+  }
+
+  #[test]
+  fn should_not_panic_with_zero_jitter() {
+    assert_eq!(add_jitter(10, 0), 10);
   }
 
   #[test]
