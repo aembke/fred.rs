c9973c951122095f04560c1b35cc3103df503ec1
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2024-03-21 11:16:01 +0300

    AFIT Slowlog && sorted sets

--- src/commands/interfaces/slowlog.rs
+++ src/commands/interfaces/slowlog.rs
@@ -3,34 +3,34 @@ use crate::{
   interfaces::{ClientLike, RedisResult},
   types::FromRedis,
 };
+use futures::Future;
 
 /// Functions that implement the [slowlog](https://redis.io/commands#server) interface.
-#[async_trait]
 pub trait SlowlogInterface: ClientLike + Sized {
   /// This command is used to read the slow queries log.
   ///
   /// <https://redis.io/commands/slowlog#reading-the-slow-log>
-  async fn slowlog_get<R>(&self, count: Option<i64>) -> RedisResult<R>
+  fn slowlog_get<R>(&self, count: Option<i64>) -> impl Future<Output = RedisResult<R>> + Send
   where
     R: FromRedis,
   {
-    commands::slowlog::slowlog_get(self, count).await?.convert()
+    async move { commands::slowlog::slowlog_get(self, count).await?.convert() }
   }
 
   /// This command is used to read length of the slow queries log.
   ///
   /// <https://redis.io/commands/slowlog#obtaining-the-current-length-of-the-slow-log>
-  async fn slowlog_length<R>(&self) -> RedisResult<R>
+  fn slowlog_length<R>(&self) -> impl Future<Output = RedisResult<R>> + Send
   where
     R: FromRedis,
   {
-    commands::slowlog::slowlog_length(self).await?.convert()
+    async move { commands::slowlog::slowlog_length(self).await?.convert() }
   }
 
   /// This command is used to reset the slow queries log.
   ///
   /// <https://redis.io/commands/slowlog#resetting-the-slow-log>
-  async fn slowlog_reset(&self) -> RedisResult<()> {
-    commands::slowlog::slowlog_reset(self).await
+  fn slowlog_reset(&self) -> impl Future<Output = RedisResult<()>> + Send {
+    async move { commands::slowlog::slowlog_reset(self).await }
   }
 }
